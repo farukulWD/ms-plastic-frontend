bfd1ea77ecbb850f9750e250ddc1c91b2794c2a9
farukulWD <farukgb1999@gmail.com>
2024-07-07 00:36:58 +0600

    complete auth functionality

--- app/(dashboard)/dashboard/add-product/page.js
+++ app/(dashboard)/dashboard/add-product/page.js
@@ -1,6 +1,9 @@
 "use client";
+import { useGetAllUserQuery } from "@/app/redux/features/auth/authApi";
 import React from "react";
 
 export default function AddProduct() {
+  const { data } = useGetAllUserQuery();
+
   return <div>add product</div>;
 }
--- app/redux/features/baseApi/baseApi.js
+++ app/redux/features/baseApi/baseApi.js
@@ -1,4 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { logOut, setUser } from "../auth/authSlice";
+import RemoveCookie from "@/components/common/RemoveCookie";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -13,8 +15,29 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const customBaseQueryWithRefreshToken = async (arg, api, extraOption) => {
+  let result = await baseQuery(arg, api, extraOption);
+
+  if (result?.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+    const data = await res.json();
+    if (data?.data?.accessToken) {
+      const user = api.getState().user.user;
+      api.dispatch(setUser({ user, token: data?.data?.accessToken }));
+      result = await baseQuery(arg, api, extraOption);
+    } else {
+      api.dispatch(logOut());
+      RemoveCookie("refreshToken");
+    }
+  }
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: customBaseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
